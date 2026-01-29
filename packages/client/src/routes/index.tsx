f99a5aa01f8c95b05af6a2def16f456a6879de7b
Maggie Cheung <mcheu.15104@gmail.com>
2026-01-29 17:54:30 -0500

    pre-1/29 checkpoint - added calendar and cards

--- packages/client/src/routes/index.tsx
+++ packages/client/src/routes/index.tsx
@@ -1,12 +1,38 @@
-import { Button } from "@/components/button";
 import { Calendar } from "@/components/calendar";
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/card";
 import { createFileRoute } from "@tanstack/react-router";
 import React from "react";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
+
+const events = [
+  {
+    instructor: "Teacher A",
+    date: "2025-06-12",
+    studio: "Modega",
+    duration: "85",
+    type: "Choreography",
+    level: "Intermediate"
+  },
+  {
+    instructor: "Teacher B",
+    date: "2025-06-12",
+    studio: "Peridance",
+    duration: "85",
+    type: "House",
+    level: "Beginner"
+  },
+  {
+    instructor: "Teacher C",
+    date: "2025-06-12",
+    studio: "ILD (Manhattan)",
+    duration: "85",
+    type: "Choreography",
+    level: "Intermediate"
+  },
+]
 
 function Index() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
@@ -17,7 +43,10 @@ function Index() {
       year: "numeric",
     })
   : ""
+
+  const classCount = events.length
   return (
+    // Root container
     <div
       className={`
         bg-white p-2 text-black
@@ -25,7 +54,7 @@ function Index() {
       `}
     >
       <h3 className="text-3xl font-bold">Dilly Dally Tracker</h3>
-      <div className="m-6">
+      <div className="m-6 w-fit">
         <Calendar
           mode="single"
           selected={date}
@@ -33,20 +62,24 @@ function Index() {
           className="rounded-md border shadow-sm w-full max-w-sm"
           captionLayout="dropdown"
         ></Calendar>
-      </div>
-
-      <div className="m-6">
-        <Card className="w-full max-w-sm">
-            <CardHeader>
-              <CardTitle>{dateTitle}</CardTitle>
-              <CardDescription>
-                Dilly Dally Log
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              sdsfd
-            </CardContent>
-          </Card>
+        <div className="flex flex-row justify-between">
+          <div className="text-m font-medium p-2">{dateTitle}</div>
+          <div className="text-m font-medium p-2">{classCount} Classes</div>
+        </div>
+        <div>
+          {events.map((event) => (
+            <Card className="w-full max-w-sm m-2">
+              <CardHeader>
+                <CardTitle>{event.type}</CardTitle>
+                <CardDescription>
+                  <div class="mb-1">{event.level}</div>
+                  <div>{event.instructor}</div>
+                  <div>{event.studio} - {event.duration} mins</div>
+                </CardDescription>
+                </CardHeader>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
