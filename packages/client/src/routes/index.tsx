b87261f35e311457368e3adc36c73b018947086f
Maggie Cheung <mcheu.15104@gmail.com>
2026-01-31 15:43:27 -0500

    added modifier, created dialog

--- packages/client/src/routes/index.tsx
+++ packages/client/src/routes/index.tsx
@@ -1,6 +1,11 @@
 import { Calendar } from "@/components/calendar";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/card";
+import { Button } from "@/components/button";
 import { createFileRoute } from "@tanstack/react-router";
+import { Building2, UserRound, Clock } from "lucide-react";
+import { useState } from "react";
+import { format } from "date-fns";
+import { AddClassDialog } from "@/components/addClass";
 import React from "react";
 
 export const Route = createFileRoute("/")({
@@ -10,7 +15,7 @@ export const Route = createFileRoute("/")({
 const events = [
   {
     instructor: "Teacher A",
-    date: "2025-06-12",
+    date: "2026-01-28",
     studio: "Modega",
     duration: "85",
     type: "Choreography",
@@ -18,15 +23,15 @@ const events = [
   },
   {
     instructor: "Teacher B",
-    date: "2025-06-12",
+    date: "2026-01-29",
     studio: "Peridance",
     duration: "85",
     type: "House",
-    level: "Beginner"
+    level: "Beginner",
   },
   {
     instructor: "Teacher C",
-    date: "2025-06-12",
+    date: "2026-01-30",
     studio: "ILD (Manhattan)",
     duration: "85",
     type: "Choreography",
@@ -35,16 +40,36 @@ const events = [
 ]
 
 function Index() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date())
+  const [date, setDate] = useState<Date | undefined>(undefined);
   const dateTitle = date
-  ? date.toLocaleDateString("en-US", {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    })
-  : ""
+  ? format(date, "yyyy-MM-dd")
+    : null;
 
-  const classCount = events.length
+  const formattedDisplayDate = date
+  ? format(date, "MMMM d, yyyy")
+  : null;
+
+  const filteredClasses = events.filter(
+    (event) => event.date === dateTitle
+  );
+
+  const modifiers = React.useMemo(() => ({
+      hasEvents: (day: Date) => {
+      return events.some((event) => {
+        const d = new Date(event.date);
+        return (
+          d.getFullYear() === day.getFullYear() &&
+          d.getMonth() === day.getMonth() &&
+          d.getDate() + 1 === day.getDate()
+        );
+      });
+    }
+  }), [events]);
+  const modifiersClass = {
+    hasEvents: 'my-booked-class',
+  }
+  const classCount = filteredClasses.length
+
   return (
     // Root container
     <div
@@ -61,25 +86,29 @@ function Index() {
           onSelect={setDate}
           className="rounded-md border shadow-sm w-full max-w-sm"
           captionLayout="dropdown"
+          modifiers={modifiers}
+          modifiersClassNames={modifiersClass}
         ></Calendar>
         <div className="flex flex-row justify-between">
-          <div className="text-m font-medium p-2">{dateTitle}</div>
+          <div className="text-m font-medium p-2">{formattedDisplayDate}</div>
           <div className="text-m font-medium p-2">{classCount} Classes</div>
         </div>
         <div>
-          {events.map((event) => (
-            <Card className="w-full max-w-sm m-2">
+          {filteredClasses.map((event) => (
+            <Card className ="my-2">
               <CardHeader>
                 <CardTitle>{event.type}</CardTitle>
                 <CardDescription>
-                  <div class="mb-1">{event.level}</div>
-                  <div>{event.instructor}</div>
-                  <div>{event.studio} - {event.duration} mins</div>
+                  <div className="px-2 py-1 bg-purple-100 text-purple-700 rounded-full inline-block mt-1 mb-8">{event.level}</div>
+                  <div className="flex items-center gap-2"><Building2></Building2>{event.studio}</div>
+                  <div className="flex items-center gap-2"><UserRound></UserRound>{event.instructor}</div>
+                  <div className="flex items-center gap-2"><Clock></Clock>{event.duration} mins</div>
                 </CardDescription>
                 </CardHeader>
             </Card>
           ))}
         </div>
+        <AddClassDialog></AddClassDialog>
       </div>
     </div>
   );
